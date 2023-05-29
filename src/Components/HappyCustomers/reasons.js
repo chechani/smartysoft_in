5be1fa967f6c5c1e2f9fbf75a90b372c55e4f45d
CA. B.C.Chechani <ca.bc.chechani@gmail.com>
2023-05-29 20:11:40 +0530

    solution added

--- src/Components/HappyCustomers/reasons.js
+++ src/Components/HappyCustomers/reasons.js
@@ -56,42 +56,42 @@ function Reasons() {
         <Grid item xs={12} md={4}>
           <Reason
             title="Implementation and Deployment"
-            description="Implement and Deploy ERPNext for organisations across different industries. Our experienced team ensures a seamless transition to ERPNext, enabling businesses to leverage its powerful features and capabilities."
+            description="Our specialization lies in the implementation and deployment of solutions handpicked by organizations from various industries."
             icon={<DevicesOtherIcon fontSize="large" style={{ color: 'green' }} />}
           />
         </Grid>
         <Grid item xs={12} md={4}>
           <Reason
             title="Customization and Integration"
-            description="Customize ERPNext to meet the specific requirements of businesses. Our expertise in integration allows us to connect ERPNext with other applications and social network platforms seamlessly."
+            description="We excel in customizing and integrating diverse technologies and solutions to meet the specific requirements of businesses."
             icon={<SentimentSatisfiedAltIcon fontSize="large" style={{ color: 'green' }} />}
           />
         </Grid>
         <Grid item xs={12} md={4}>
           <Reason
             title="Enhancement and Optimization"
-            description="Enhance and optimize existing ERPNext installations. Our team conducts a thorough analysis to identify areas for improvement and optimize the utilization of ERPNext, resulting in increased efficiency and productivity."
+            description="We specialize in identifying areas for improvement and maximizing the utilization of ERPNext, in conjunction with other technologies. "
             icon={<FavoriteBorderIcon fontSize="large" style={{ color: 'green' }} />}
           />
         </Grid>
         <Grid item xs={12} md={4}>
           <Reason
             title="Technical and Functional Consultancy"
-            description="Provide technical and functional consultancy services to businesses utilizing ERPNext. Our experts offer guidance, best practices, and recommendations to maximize the benefits of ERPNext and address specific business challenges."
+            description="We consult in utilizing ERPNext and other technologies and extend guidance, share best practices, and our recommendations."
             icon={<AddToHomeScreenIcon fontSize="large" style={{ color: 'green' }} />}
           />
         </Grid>
         <Grid item xs={12} md={4}>
           <Reason
             title="Support and Maintenance"
-            description="Ongoing support and maintenance services for ERPNext. Our dedicated team ensures the smooth operation of ERPNext systems, promptly addresses issues, and offers timely assistance"
+            description="We provide ongoing services for ERPNext and other deployed technologies. Our primary focus is on ensuring the smooth operation of these systems"
             icon={<PersonalVideoIcon fontSize="large" style={{ color: 'green' }} />}
           />
         </Grid>
         <Grid item xs={12} md={4}>
           <Reason
             title="Training and Knowledge Transfer"
-            description="Train and knowledge transfer services to empower businesses to utilize ERPNext fully. Through our training, clients gain a deep understanding of ERPNext functionalities and acquire the skills"
+            description="We offer comprehensive training and knowledge transfer services to empower businesses in utilizing technologies to their fullest potential"
             icon={<AddShoppingCartIcon fontSize="large" style={{ color: 'green' }} />}
           />
         </Grid>
