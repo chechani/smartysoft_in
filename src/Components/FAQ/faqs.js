a84922d09b6da679748a4b8639b28caaae0b4c8a
CA. B.C.Chechani <ca.bc.chechani@gmail.com>
2023-06-03 22:15:51 +0530

    solution added

--- src/Components/FAQ/faqs.js
+++ src/Components/FAQ/faqs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 import { Card, CardContent, Grid, Box } from "@mui/material";
 import Accordion from "@mui/material/Accordion";
@@ -7,157 +7,123 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-export const FAQ = () => {
+export const FAQ = ({ category }) => {
+  const [faqData, setFaqData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [expanded, setExpanded] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://smartysoftware.in/api/method/professional.web.get_smarty_faq?category=${encodeURIComponent(category)}`);
+        const data = await response.json();
+
+        if (response.ok) {
+          if (Array.isArray(data.data)) {
+            setFaqData(data.data);
+            setExpanded({}); // Reset expanded state when FAQs change
+          } else {
+            setError("Invalid response format: FAQs not found");
+          }
+        } else {
+          setError("Error fetching FAQs: " + data.message);
+        }
+      } catch (error) {
+        setError("Error fetching FAQs: " + error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [category]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (faqData.length === 0) {
+    return <p>No FAQs found.</p>;
+  }
+
+  const handleAccordionToggle = (faqName) => {
+    setExpanded((prevState) => ({
+      ...prevState,
+      [faqName]: !prevState[faqName],
+    }));
+  };
+
   return (
     <>
       <Box
         sx={{
           textAlign: "center",
           fontSize: "32px",
-          fontWeight:"bold",
-          p:2,
-          mt:5,
-                   
-        
+          fontWeight: "bold",
+          p: 2,
+          mt: 5,
         }}
       >
         Frequently Asked Questions
       </Box>
 
-      {/* //////////// */}
-      <Box sx={{width:"95%", textAlign:"center",pb:2,ml:{xs:1,sm:5} ,position:"relative",marginTop:"50px"}}>
+  
+      <Box
+        sx={{
+          width: "95%", // Increase the width to "100%" for more space
+          textAlign: "center",
+          pb: 2,
+          ml: { xs: 1, sm: 5 },
+          position: "relative",
+          marginTop: "20px",
+        }}
+      >
         <Card variant="outlined">
           <CardContent>
             <Grid container spacing={0}>
-            <Grid item xs={12} md={6} style={{ height: "350px",marginTop:"40px" }}>
-  <img src="https://smartysoftware.in/en/img/features/faq-qa.png" alt="mmm" style={{ width: "auto", height: "100%"}} />
-</Grid>
+              <Grid item xs={12} md={6}>
+                <img
+                  src="https://smartysoftware.in/en/img/features/faq-qa.png"
+                  alt="mmm"
+                  style={{ width: "80%", height: "auto", maxHeight: "350px" }} // Set fixed width, height, and maxHeight for the image
+                />
+              </Grid>
 
               <Grid item xs={12} md={6}>
                 <Box>
-                  <Accordion>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel2a-content"
-                      id="panel2a-header"
+                  {faqData.map((faq) => (
+                    <Accordion
+                      key={faq.name}
+                      expanded={expanded[faq.name]} // Pass the expanded state for each accordion
+                      onChange={() => handleAccordionToggle(faq.name)}
                     >
-                      <Typography>
-                        <Typography sx={{fontWeight:"bold",fontSize:"20px",textAlign:"start"}}>
-                          <DoubleArrowIcon />
-                          Can I schedule or automate the data import process from Excel to Tally
-                        </Typography>
-                      </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography
-                        style={{ textAlign: "start" }}
+                      <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls={`panel-${faq.name}-content`}
+                        id={`panel-${faq.name}-header`}
                       >
-                        <Typography style={{ color: "grey",fontSize:"18px" }}>
-                        Yes, Once Excel Sheet is uploaded after maping to our standard templates, it will upload all rows automatically. 
-                        It will mark imported rows as "Y" and Failed as "N". 
-                        </Typography>
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                  <Accordion sx={{mt:2}}>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel2a-content"
-                      id="panel2a-header"
-                    >
-                      <Typography>
-                        <Typography sx={{fontWeight:"bold",fontSize:"20px",textAlign:"start"}}>
-                          <DoubleArrowIcon />
-                          What types of data can I import from Excel to Tally?
-                        </Typography>
-                      </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography
-                        style={{textAlign: "start" }}
-                      >
-                        <Typography style={{ color: "grey",fontSize:"18px" }}>
-                        It can import any voucer type and you need to to design templates
-                        as per formats of our standrad templates. Our assistance may requires and we also undertake
-                        separate Template Development Services from your formats at a cost of Rs. 1000 per Tempalte
-                        </Typography>
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                  <Accordion sx={{mt:2}}>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel2a-content"
-                      id="panel2a-header"
-                    >
-                      <Typography>
-                        <Typography sx={{fontWeight:"bold",fontSize:"20px",textAlign:"start"}}>
-                          <DoubleArrowIcon />
-                          How do I map fields between Excel and Tally for a data import process?
-                        </Typography>
-                      </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography
-                        style={{ textAlign: "start" }}
-                      >
-                        <Typography style={{ color: "grey",fontSize:"18px"}}>
-                        Its a simple process for existing Excel worker. They just need to map your sheet coulumns to our sheet.
-                        Information not conatined in your sheet and requires to import data then default values are marked.
-                        </Typography>
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                  <Accordion sx={{mt:2}}>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel2a-content"
-                      id="panel2a-header"
-                    >
-                      <Typography>
-                        <Typography sx={{fontWeight:"bold",fontSize:"20px",textAlign:"start"}}>
-                          <DoubleArrowIcon />
-                          What are the best practices for formatting Excel data to import?
-                        </Typography>
-                      </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography
-                        style={{ textAlign: "start" }}
-                      >
-                        <Typography style={{ color: "grey",fontSize:"18px" }}>
-                        Any Excel worker can map very easily columns of different sheets . We also provide
-                        training to do this when you buy it. 
+                        <Typography>
+                          <Typography
+                            sx={{  fontSize: "18px", textAlign: "start" }}
+                          >
+                            <DoubleArrowIcon />
+                            {faq.question}
                           </Typography>
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                  <Accordion sx={{mt:2}}>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel2a-content"
-                      id="panel2a-header"
-                    >
-                      <Typography>
-                        <Typography sx={{fontWeight:"bold",fontSize:"20px",textAlign:"start"}}>
-                          <DoubleArrowIcon />
-                          How do I handle errors in my Excel data during the import process?
                         </Typography>
-                      </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography
-                        style={{textAlign: "start" }}
-                      >
-                        <Typography style={{ color: "grey",fontSize:"18px"}}>
-                        Transaction having errors are marked as No in Excel sheet so one can easily find
-                        those transactions and can fix easily . We also help and provide assistance to find out 
-                        issue and resove it. 
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <Typography style={{ textAlign: "start" }}>
+                          <Typography
+                            style={{ color: "grey", fontSize: "16px" }}
+                          >
+                            {faq.answer}
+                          </Typography>
                         </Typography>
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-               </Box>
+                      </AccordionDetails>
+                    </Accordion>
+                  ))}
+                </Box>
               </Grid>
             </Grid>
           </CardContent>
